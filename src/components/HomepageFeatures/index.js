1c01ecda58b150e92b65f56eb904ee1fe59b00a8
Wasim Tamboli <wtamboli4@gmail.com>
2024-02-27 22:52:10 +0530

    fix: homepage
    Signed-off-by: Wasim Tamboli <wtamboli4@gmail.com>

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,7 +5,8 @@ import styles from './styles.module.css';
 const FeatureList = [
   {
     title: 'Map Tiles',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    Svg: require('@site/static/img/map-tiles.svg').default,
+    URL: 'docs/map-tiles/',
     description: (
       <>
         Vector and raster maps with GL styles. Server-side rendering by MapLibre GL Native.
@@ -15,7 +16,8 @@ const FeatureList = [
   },
   {
     title: 'Routing Engine',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    Svg: require('@site/static/img/routing.svg').default,
+    URL: 'docs/routing/',
     description: (
       <>
         Routing engine and accompanying libraries includes tools like tour optimization 
@@ -25,7 +27,8 @@ const FeatureList = [
   },
   {
     title: 'Places',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    Svg: require('@site/static/img/geocode.svg').default,
+    URL: 'docs/places/',
     description: (
       <>
         Search engine for places. It turns addresses and place names into geographic coordinates, 
@@ -35,14 +38,18 @@ const FeatureList = [
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({Svg, URL, title, description}) {
   return (
     <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
+      <a href={URL}>
+        <div className="text--center">
+          <Svg className={styles.featureSvg} role="img" />
+        </div>
+      </a>
       <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
+        <a href={URL}>  
+          <Heading as="h3">{title}</Heading>
+        </a>
         <p>{description}</p>
       </div>
     </div>
